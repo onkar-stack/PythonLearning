ead44b85793de77db3c878f8a583607420bbb9c1
onkar-n <onkarnath05@gmail.com>
2020-02-23 16:07:29 +0530

    what is class

---
+++ oops/durgaSir/session1.py
@@ -0,0 +1,32 @@
+# in python everything is object
+# class: blueprint of plan
+# Object: pPysical existence of plan
+
+
+# student
+
+# name
+# roll
+# age
+# Above mentionwd are attributes/properties   
+
+# read 
+
+#behavior is represented by method
+
+# how to define class
+
+#class classname:
+	# document
+	# variable:
+		# instance variable
+		# static variable
+		# loacal variable
+	# menthod:
+		# instance method
+		# static method
+		# class method
+
+
+
+
