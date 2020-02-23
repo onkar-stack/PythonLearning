b6224bc909da25f72f7e6a185887f1a19f43e43f
onkar-n <onkarnath05@gmail.com>
2020-02-23 16:40:35 +0530

    how to define class

--- oops/durgaSir/session1.py
+++ oops/durgaSir/session1.py
@@ -1,8 +1,6 @@
 # in python everything is object
 # class: blueprint of plan
 # Object: pPysical existence of plan
-
-
 # student
 
 # name
@@ -19,14 +17,39 @@
 #class classname:
 	# document
 	# variable:
-		# instance variable
-		# static variable
-		# loacal variable
+		# instance variable (object level var)
+		# static variable (class level var)
+		# loacal variable (method level var)
 	# menthod:
 		# instance method
 		# static method
 		# class method
+	# def __init__(self): constructor
+
+
+	# if a fuction is declared inside a class it is called method.
+	# if a fuction is declared outside a class it is called fuction.
+
+	# reference variable
 
 
 
 
+class student:
+	"developeped by onkar"
+	def __init__(self, name, rollnum, marks):
+		self. name = name
+		self.rollnum = rollnum
+		self.marks = marks
+
+	def result(self):
+		if self.marks > 300:
+			print("pass")
+		else:
+			print("fail")
+
+s1 = student("onkar", 101, 500)
+print(s1.name)
+print(s1.rollnum)
+print(s1.marks)
+s1.result()
